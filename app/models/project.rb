c016071e27b5e92810eba6e9985bb71b8df16696
AlisonAO <alison_opeloyeru@hotmail.co.uk>
2017-10-10 16:10:41 +0100

    edited validation message

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@ class Project < ApplicationRecord
 	has_many :users, through: :user_project
 
 	validates :title, :creator, :image, :description, :readme, :tags, presence: true
-	validates :description, length: { minimum: 10, message: "Sorry your description is too short"}
+	validates :description, length: { minimum: 10, message: "is too short"}
 
 end
 
