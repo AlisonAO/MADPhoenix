7b40259fc86ae9dfe1247c99e6b52cf8ad39a880
AlisonAO <alison_opeloyeru@hotmail.co.uk>
2017-10-10 14:41:32 +0100

    added validation options to the model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,9 @@
 class Project < ApplicationRecord
 	has_many :user_project
 	has_many :users, through: :user_project
+
+	validates :title, :creator, :image, :description, :readme, :tags, presence: true
+	validates :description, length: { minimum: 10, message: "Sorry your description is too short"}
+
 end
+
