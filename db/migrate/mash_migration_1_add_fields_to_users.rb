f4b46c471aa43071f1d747406a6f5be469edb5dc
MashR3 <michaelash17@googlemail.com>
2017-10-10 15:12:16 +0100

    Reverted Changes for new guide

--- db/migrate/mash_migration_1_add_fields_to_users.rb
+++ db/migrate/mash_migration_1_add_fields_to_users.rb
@@ -1,11 +1,10 @@
 class AddFieldsToUsers < ActiveRecord::Migration[5.1]
 	def change 
-		add_column: :users, :first_name, :string
-		add_column: :users, :last_name, :string
-		add_column: :users, :github_link, :string
-		add_column: :users, :linkedin_link, :string
-		add_column: :users, :position, :string
-		add_column: :users, :profile_pic, :text
+		add_column :users, :first_name, :string
+		add_column :users, :last_name, :string
+		add_column :users, :github_link, :string
+		add_column :users, :linkedin_link, :string
+		add_column :users, :position, :string
+		add_column :users, :profile_pic, :text
 	end
 end
-
