1e9c9f714e9b4bc322f2a8d81eb0ab1d3c2bbb1b
djonesdev <domajones@outlook.com>
2017-10-11 14:43:21 +0100

    A safeish space

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,8 @@
 class Project < ApplicationRecord
-	
+	has_attached_file :image
+
+	validates_attachment :image,
+                     content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 
 	validates :title, :creator, :image, :description, :readme, :tags, presence: true
 	validates :description, length: { minimum: 10, message: "is too short"}
