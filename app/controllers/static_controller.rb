83aaa5de18ea0a0412160dbea6fba2f15713ede7
AlisonAO <alison_opeloyeru@hotmail.co.uk>
2017-10-14 17:28:05 +0100

    edited controller

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,15 +1,6 @@
-class StaticController < ApplicationController 
+# class StaticController < ApplicationController 
 
-	def home
-		if signed_in? == true
-			if current_user.admin
-			  @projects = Project.all
-			else
-			  @projects = Project.where(approved: true)
-			end
-		else
-			@projects = Project.where(approved: true)
-		end 
-		render template: "projects/index"
-	end
-end
+# # 	def home
+# # 		render template: "projects/index"
+# # 	end
+# # end
