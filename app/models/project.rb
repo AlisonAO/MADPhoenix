e407e84755de6586ad25fcbd09b38259f23bce79
AlisonAO <alison_opeloyeru@hotmail.co.uk>
2017-10-14 22:30:14 +0100

    removed duo validation error messages

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,13 +8,13 @@ class Project < ApplicationRecord
 
 	validates_attachment :image,
                      content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
-
-	validates :title, :creator, :image, :description, :readme, :tags, presence: true
+	validates :description, presence: { message: " "}
+	validates :title, :creator, :image, :readme, :tags, presence: true
 	validates :description, length: { minimum: 10, message: "is too short"}
 
 	def self.search(search)
   		where("title ILIKE ? OR creator ILIKE ? OR description ILIKE ? OR tags ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%") 
-end
+	end
 end
 
 
