858365b234e9691b7d6d1f8d3474b293654a3a66
AlisonAO <alison_opeloyeru@hotmail.co.uk>
2017-10-14 22:19:55 +0100

    fixed error

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -8,8 +8,7 @@ module DeviseHelper
   def devise_error_messages!
     return "" if resource.errors.empty?
 
-    messages = resource.errors.details.keys.map { |attr|      
-      content_tag(:li, resource.errors.full_messages_for(attr).first) }.join
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
     sentence = I18n.t("errors.messages.not_saved",
                       count: resource.errors.count,
                       resource: resource.class.model_name.human.downcase)
