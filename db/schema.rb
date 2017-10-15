4d9dbe2f9359ada445daf654dac0dfff81ef7605
MashR3 <michaelash17@googlemail.com>
2017-10-15 15:45:30 +0100

    I'm so done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013134621) do
+ActiveRecord::Schema.define(version: 20171015132404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20171013134621) do
     t.integer "image_file_size"
     t.datetime "image_updated_at"
     t.boolean "approved"
+    t.string "gh_link"
   end
 
   create_table "users", force: :cascade do |t|
